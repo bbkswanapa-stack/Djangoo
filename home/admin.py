b031fa21b03ef0bf362ca998116c64df4c07cc90
unknown <bbkswanapa@gmail.com>
2026-05-17 15:51:08 +0545

    create db table. insert data, CRUD using ORM

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from home.models import Student
 
 # Register your models here.
+
+# admin.site.register(Student)
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'DOB', 'number', 'is_active']
+    search_fields = ['name', 'DOB']
+    list_filter = ['is_active']
--- home/models.py
+++ home/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=50, null=True, blank= True, help_text="Student full name", verbose_name="Student Name")
+    DOB = models.DateField()
+    number = models.PositiveIntegerField(default=0)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'{self.name}'
+    class Meta:
+     db_table = "student"  #To overwrite the name of table
